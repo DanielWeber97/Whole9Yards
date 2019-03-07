c7c12585845c042420c8f2e1658f442397e9a365
DanielWeber97 <daniel.weber.443@gmail.com>
2019-03-07 13:06:14 -0500

    Updated image for splash screen

--- app/src/main/java/com/example/whole9yards/MainActivity.java
+++ app/src/main/java/com/example/whole9yards/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.whole9yards;
 
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.CountDownTimer;
-import android.os.PersistableBundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,27 +19,132 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView timerTV;
+    private TextView startStopTV;
+    private long startTime;
+    private int minute = 0;
+    private int isClicked = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
 
-//       final Intent toHome = new Intent(this, HomeActivity.class);
-//
-//        new CountDownTimer(700, 1000){
-//
-//            @Override
-//            public void onTick(long l) {
-//
-//            }
-//
-//            @Override
-//            public void onFinish() {
-//
-//                startActivity(toHome);
-//            }
-//        }.start();
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+        //showPopup();
     }
 
+
+    public void startOnClick(View v){
+
+
+        Timer timer = new Timer();
+
+        if(isClicked == 0){
+
+            startStopTV = findViewById(R.id.startStopTV);
+            startStopTV.setText("Stop Job");
+
+
+
+            startTime = System.currentTimeMillis();
+
+
+
+
+
+            timer.scheduleAtFixedRate(new TimerTask() {
+
+
+
+                @Override
+                public void run() {
+
+                    //long minutesStart = TimeUnit.MILLISECONDS.toMinutes(startTime);
+                    //long secondsStart = TimeUnit.MILLISECONDS.toSeconds(startTime);
+                    long secondsCalc = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startTime);
+
+                    Log.v("MYTAG", secondsCalc + "");
+
+                    timerTV = findViewById(R.id.timer);
+                    if(secondsCalc % 60 == 0){
+                        minute++;
+                    }
+                    if(minute>=10){
+                        if(secondsCalc%60 < 10) {
+                            timerTV.setText(minute + ":" +  "0"+secondsCalc % 60 );
+                        } else{
+                            timerTV.setText(minute + ":" + secondsCalc % 60 + "");
+                        }
+                    } else{
+                        if(secondsCalc%60 < 10) {
+                            timerTV.setText("0"+minute + ":" + "0" +secondsCalc % 60);
+                        } else{
+                            timerTV.setText("0"+minute + ":" + secondsCalc % 60 + "");
+                        }
+
+                    }
+
+                    // Your database code here
+                }
+            }, 1000, 1000);    /// set to run every second
+
+
+            isClicked = 1;
+
+        }
+
+        else if(isClicked == 1){
+
+            //cancel timer
+            timer.cancel();
+
+            //reset instance variable
+            minute = 0;
+
+            //rest boolean variable
+            isClicked = 0;
+
+            //reset step
+            timerTV.setText("00:00");
+            startStopTV.setText("Start Job");
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+    public void showPopup(){
+        String[] singleChoiceItems = {"Thanks for working today!\nA confirmation email has been sent to your boss and client."};
+        int itemSelected = 0;
+        android.app.Dialog dialog = new AlertDialog.Builder(this)
+                .setTitle("All Done!")
+                .setSingleChoiceItems(singleChoiceItems, itemSelected, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int selectedIndex) {
+
+                    }
+                })
+                .setPositiveButton("Ok", null)
+                .setNegativeButton("Cancel", null)
+                .show();
+
+        //dialog.show();
+    }
 
 }
