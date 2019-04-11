aaa0009dd049e7561150ad499a66b085ce6c6462
DanielWeber97 <daniel.weber.443@gmail.com>
2019-04-11 16:11:35 -0400

    small dependencies

--- app/src/main/java/com/example/whole9yards/Client.java
+++ app/src/main/java/com/example/whole9yards/Client.java
@@ -1,14 +1,24 @@
 package com.example.whole9yards;
 
 public class Client {
+    String clientId;
     String clientName;
     String clientNumber;
     String clientAddress;
 
-    public Client(String clientName, String clientNumber, String clientAddress) {
+    public Client(String clientId, String clientName, String clientNumber, String clientAddress) {
+        this.clientId = clientId;
         this.clientName = clientName;
         this.clientNumber = clientNumber;
         this.clientAddress = clientAddress;
+    }
+
+   // public Client(){
+
+    //}
+
+    public String getClientId(){
+        return clientId;
     }
 
     public String getClientName() {
